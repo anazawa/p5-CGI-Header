8fc79d1d519554c504954af13348917fd216bc12
Ryo Anazawa <anazawa@cpan.org>
2013-05-19 06:03:03 +0900

    Checking in changes prior to tagging of version 0.53.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 09883aa..3f95d09 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,7 @@
     Revision history for Perl extension CGI::Header.
    
    +0.53 (1.00-RC6) May 19th, 2013
    +
       [DOCUMENTATION]
    
         - update EXAMPLES

--- lib/CGI/Header.pm
+++ lib/CGI/Header.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 use Carp qw/croak/;
 
-our $VERSION = '0.52';
+our $VERSION = '0.53';
 
 my %Property_Alias = (
     'content-type'  => 'type',
@@ -161,7 +161,7 @@ CGI::Header - Handle CGI.pm-compatible HTTP header properties
 
 =head1 VERSION
 
-This document refers to CGI::Header version 0.52.
+This document refers to CGI::Header version 0.53.
 
 =head1 DEPENDENCIES
 
