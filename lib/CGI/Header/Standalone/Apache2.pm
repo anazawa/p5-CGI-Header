685b5afa34e4336c4cdcd000165badb758bd70bf
Ryo Anazawa <anazawa@cpan.org>
2013-05-20 22:19:09 +0900

    no need to use send_cgi_header()

--- lib/CGI/Header/Standalone/Apache2.pm
+++ lib/CGI/Header/Standalone/Apache2.pm
@@ -4,8 +4,26 @@ use warnings;
 use parent 'CGI::Header::Standalone';
 
 sub finalize {
-    my $self = shift;
-    $self->query->r->send_cgi_header( $self->SUPER::finalize );
+    my $self        = shift;
+    my $headers     = $self->as_arrayref;
+    my $r           = $self->query->r;
+    my $headers_out = $r->headers_out;  
+
+    for ( my $i = 0; $i < @$headers; $i += 2 ) {
+        my $field = $headers->[$i];
+        my $value = $self->_process_newline( $headers->[$i+1] );
+
+        if ( $field eq 'Content-Type' ) {
+            $r->content_type( $value );
+        }
+        elsif ( $field eq 'Content-length' ) {
+            $r->set_content_length( $value );
+        }
+        else {
+            $headers_out->add( $field => $value );
+        }
+    }
+
     q{};
 }
 
