d0a05297d1ae1b96de5e2731430e6f23b83b7fc4
Ryo Anazawa <anazawa@cpan.org>
2013-07-31 05:10:13 +0900

    use #set

--- examples/lib/CGI/Header/Extended.pm
+++ examples/lib/CGI/Header/Extended.pm
@@ -5,16 +5,16 @@ use parent 'CGI::Header';
 
 sub merge {
     my ( $self, @args ) = @_;
-    my $header = $self->header;
 
     if ( @args == 1 ) {
         my %header = %{ $args[0] };
+        my $header = $self->header;
         ref( $self )->new( header => \%header ); # rehash %header
         %$header = ( %$header, %header );
     }
     else {
         while ( my ($key, $value) = splice @args, 0, 2 ) {
-            $header->{ $self->normalize($key) } = $value; # overwrite 
+            $self->set( $key => $value );
         }
     }
 
