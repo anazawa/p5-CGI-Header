eee9211667795aa91d6fed254ae585dd6bd83251
Ryo Anazawa <anazawa@cpan.org>
2013-07-31 23:59:04 +0900

    rename @args to @pairs

--- examples/lib/CGI/Header/Extended.pm
+++ examples/lib/CGI/Header/Extended.pm
@@ -4,24 +4,24 @@ use warnings;
 use parent 'CGI::Header';
 use Carp qw/croak/;
 
-sub set {
-    my ( $self, @args ) = @_;
-    my $header = $self->header;
-
-    croak "Odd number of elements passed to 'set'" if @args % 2;
-
-    my @values;
-    while ( my ($key, $value) = splice @args, 0, 2 ) {
-        push @values, $self->SUPER::set( $key => $value );
-    }
-
-    @values;
-}
-
 sub get {
     my ( $self, @keys ) = @_;
     my @values = map { $self->SUPER::get($_) } @keys;
     wantarray ? @values : $values[-1];
+}
+
+sub set {
+    my ( $self, @pairs ) = @_;
+    my $header = $self->header;
+
+    croak "Odd number of elements passed to 'set'" if @pairs % 2;
+
+    my @values;
+    while ( my ($key, $value) = splice @pairs, 0, 2 ) {
+        push @values, $self->SUPER::set( $key => $value );
+    }
+
+    @values;
 }
 
 sub delete {
