1a5fcb4f48b0048c10c790603b5f9a6685e9a9e3
Ryo Anazawa <anazawa@cpan.org>
2013-07-31 23:56:27 +0900

    remove CGI::Header::Extended#merge and #replace

--- examples/lib/CGI/Header/Extended.pm
+++ examples/lib/CGI/Header/Extended.pm
@@ -2,6 +2,21 @@ package CGI::Header::Extended;
 use strict;
 use warnings;
 use parent 'CGI::Header';
+use Carp qw/croak/;
+
+sub set {
+    my ( $self, @args ) = @_;
+    my $header = $self->header;
+
+    croak "Odd number of elements passed to 'set'" if @args % 2;
+
+    my @values;
+    while ( my ($key, $value) = splice @args, 0, 2 ) {
+        push @values, $self->SUPER::set( $key => $value );
+    }
+
+    @values;
+}
 
 sub get {
     my ( $self, @keys ) = @_;
@@ -13,28 +28,6 @@ sub delete {
     my ( $self, @keys ) = @_;
     my @values = map { $self->SUPER::delete($_) } @keys;
     wantarray ? @values : $values[-1];
-}
-
-sub merge {
-    my ( $self, @args ) = @_;
-
-    if ( @args == 1 ) {
-        my $header = $self->header;
-        my $other = ref( $self )->new( header => { %{$args[0]} } );
-        %$header = ( %$header, %{ $other->header } );
-    }
-    else {
-        while ( my ($key, $value) = splice @args, 0, 2 ) {
-            $self->set( $key => $value ); # overwrite
-        }
-    }
-
-    $self;
-}
-
-sub replace {
-    my $self = shift;
-    $self->clear->merge(@_);
 }
 
 1;
